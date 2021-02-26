b36d197f38f68850ff94875f7ce082777f98c972
rghust21 <rghust21@colby.edu>
2021-02-26 14:54:19 -0500

    HW7 script

--- HW7.R
+++ HW7.R
@@ -1,3 +1,17 @@
 ### hw 7
+#load data
+dat <- read.csv("https://raw.githubusercontent.com/colby-es218-2021/hw07-rghust21/main/SP2021.csv?token=AS457W7VZ33KQWLJ27WVDJLAHAUYS")
 
-apple apple 
+#load librarys
+library(lubridate)
+library(dplyr)
+library(forcats)
+
+#using casewhen
+dat1 <- dat %>%
+  mutate(Overenrolled <- case_when(Reg > Max ~ "0",
+                                   Reg == Max ~ "C",
+                                   Reg < Max ~ "U",
+                                   is.na(Reg) | is.na(Max) ~ NA_character_),
+         stime =  str_split_fixed(Times, "-", 2 ) [ ,1],
+         etime =  str_split_fixed(Times, "-", 2 ) [ ,2])

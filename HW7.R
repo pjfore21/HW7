136c9066fb68c67f0a9adc48dd135de59e1f1e40
rghust21 <rghust21@colby.edu>
2021-02-26 15:00:46 -0500

    edited version

--- HW7.R
+++ HW7.R
@@ -1,11 +1,13 @@
 ### hw 7
 #load data
-dat <- read.csv("https://raw.githubusercontent.com/colby-es218-2021/hw07-rghust21/main/SP2021.csv?token=AS457W7VZ33KQWLJ27WVDJLAHAUYS")
+dat <- read.csv("https://raw.githubusercontent.com/colby-es218-2021/hw07-rghust21/main/SP2021.csv?token=AS457W2G7MSTBYAVGK65GNTAHFJU2")
 
 #load librarys
 library(lubridate)
 library(dplyr)
 library(forcats)
+library(stringr)
+
 
 #using casewhen
 dat1 <- dat %>%
@@ -15,3 +17,4 @@ dat1 <- dat %>%
                                    is.na(Reg) | is.na(Max) ~ NA_character_),
          stime =  str_split_fixed(Times, "-", 2 ) [ ,1],
          etime =  str_split_fixed(Times, "-", 2 ) [ ,2])
+
